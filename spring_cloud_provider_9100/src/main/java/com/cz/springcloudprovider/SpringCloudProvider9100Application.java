909163959ab947f47c5d12f00818851ca28d1043
aliancao <aliancao@gvt861.com>
2020-10-13 15:45:49 +0800

    引入SKD，修改基础扫描包

--- spring_cloud_provider_9100/src/main/java/com/cz/springcloudprovider/SpringCloudProvider9100Application.java
+++ spring_cloud_provider_9100/src/main/java/com/cz/springcloudprovider/SpringCloudProvider9100Application.java
@@ -3,9 +3,10 @@ package com.cz.springcloudprovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cz"})
 public class SpringCloudProvider9100Application {
 
     public static void main(String[] args) {
