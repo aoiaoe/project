6df6abc0ba8f99aaf291f182ab59bc5ac1e2b3e5
aoiaoe <aoiaue@163.com>
2020-10-24 14:02:51 +0800

    修改feign请求header

--- spring_cloud_consumer_feign_9202/src/main/java/com/cz/springcloudconsumerfeign/config/FeignInterceptor.java
+++ spring_cloud_consumer_feign_9202/src/main/java/com/cz/springcloudconsumerfeign/config/FeignInterceptor.java
@@ -3,9 +3,11 @@ package com.cz.springcloudconsumerfeign.config;
 import com.cz.springcloud.enums.HttpHeaderEnum;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 修改feign请求,如增删header,修改body
  * @author alian
  * @date 2020/10/13 下午 2:48
  * @since JDK8
@@ -13,8 +15,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignInterceptor implements RequestInterceptor {
 
+    @Value("${server.port:9202}")
+    private int port;
+
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        requestTemplate.header(HttpHeaderEnum.SENDER_CLIENT.name(), "FEIGN_CLIENT_9202");
+        requestTemplate.header(HttpHeaderEnum.SENDER_CLIENT.name(), "FEIGN_CLIENT_" + port);
     }
 }
