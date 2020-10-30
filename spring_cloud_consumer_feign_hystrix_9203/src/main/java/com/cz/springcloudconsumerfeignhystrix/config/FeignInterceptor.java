2941f8894ac080ab2822730ff236472720e2e22e
aliancao <aliancao@gvt861.com>
2020-10-30 18:27:51 +0800

    feign拦截器

---
+++ spring_cloud_consumer_feign_hystrix_9203/src/main/java/com/cz/springcloudconsumerfeignhystrix/config/FeignInterceptor.java
@@ -0,0 +1,24 @@
+package com.cz.springcloudconsumerfeignhystrix.config;
+
+import com.cz.springcloud.enums.HttpHeaderEnum;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author alian
+ * @date 2020/10/13 下午 2:48
+ * @since JDK8
+ */
+@Configuration
+public class FeignInterceptor implements RequestInterceptor {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        requestTemplate.header(HttpHeaderEnum.SENDER_CLIENT.name(), "FEIGN_CLIENT_" + port);
+    }
+}
