4521e5aa1556bf089a70003b52039411a6c24e32
aliancao <aliancao@gvt861.com>
2020-10-30 18:03:49 +0800

    feign客户端定制化路由策略

---
+++ spring_cloud_consumer_feign_9202/src/main/java/com/cz/feignconfig/RoundRobinRuleConfig.java
@@ -0,0 +1,20 @@
+package com.cz.feignconfig;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author alian
+ * @date 2020/10/30 下午 5:55
+ * @since JDK8
+ */
+@Configuration
+public class RoundRobinRuleConfig {
+
+    @Bean
+    public IRule roundRobin(){
+        return new RoundRobinRule();
+    }
+}
---
+++ spring_cloud_consumer_feign_9202/src/main/java/com/cz/springcloudconsumerfeign/feign/EntityFeignRoundRobinClient.java
@@ -0,0 +1,14 @@
+//package com.cz.springcloudconsumerfeign.feign;
+//
+//import com.cz.feignconfig.RoundRobinRuleConfig;
+//import com.cz.springcloud.api.EntityApi;
+//import org.springframework.cloud.openfeign.FeignClient;
+//
+///**
+// * @author alian
+// * @date 2020/10/13 下午 12:07
+// * @since JDK8
+// */
+//@FeignClient(name = "provider", configuration = RoundRobinRuleConfig.class)
+//public interface EntityFeignRoundRobinClient extends EntityApi {
+//}
