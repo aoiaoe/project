7c9923c5f198723a5318015d05808ef974819f14
aliancao <aliancao@gvt861.com>
2020-11-12 16:19:29 +0800

    线程的中断测试

--- java_base/src/main/java/com/cz/thread/ThreadInterruptDemo.java
+++ java_base/src/main/java/com/cz/thread/ThreadInterruptDemo.java
@@ -2,6 +2,18 @@ package com.cz.thread;
 
 /**
  * 线程的中断测试
+ * interrupt（）是给线程设置中断标志；interrupted（）是检测中断并清除中断状态；isInterrupted（）只检测中断。
+ * 还有重要的一点就是interrupted（）作用于当前线程，
+ * interrupt（）和isInterrupted（）作用于此线程，即代码中调用此方法的实例所代表的线程。
+ *
+ * interrupt()：打一个中断标记，并没有实际去中断一个线程。
+ * isInterrupted()：如果调用了interrupt()方法，则这里会输出true，表示当前线程被中断过。
+ * interrupted()：静态方法。如果调用了interrupt()方法，则这里会输出true，表示当前线程被中断过，但是这个方法比上面的isInterrupted()方法多做了一件事，那就是线程复位，
+ *                  也就是说，连续两次调用interrupted()方法后，第一次为true，第二次就会变回false。
+ *
+ * 为什么需要对线程复位？ 我们设想这么一种场景，假如我们规定收到3次线程中断要求的时候，即使线程没有执行完毕这时候也需要直接返回，
+ * 那么线程如果不复位，我们就没办法知道当前线程进行过多少次中断(不考虑同时多次中断的情况)，
+ * 因为中断过一次，一直是true；而有了线程复位，我们就只需要判断3次都是true就说明至少被中断过3次。
  * @author alian
  * @date 2020/11/12 上午 11:48
  * @since JDK8
