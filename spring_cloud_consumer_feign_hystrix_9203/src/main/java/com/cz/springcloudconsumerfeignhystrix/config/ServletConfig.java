eeb79b79cf59a280a1e34b1a0e5207dcbbf443cb
aliancao <aliancao@gvt861.com>
2020-11-12 11:13:32 +0800

    配置servlet打开/hystrix.stream端点

---
+++ spring_cloud_consumer_feign_hystrix_9203/src/main/java/com/cz/springcloudconsumerfeignhystrix/config/ServletConfig.java
@@ -0,0 +1,26 @@
+package com.cz.springcloudconsumerfeignhystrix.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author alian
+ * @date 2020/11/2 上午 10:30
+ * @since JDK8
+ */
+@Configuration
+public class ServletConfig {
+
+    @Bean
+    public ServletRegistrationBean hystrixStreamServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
