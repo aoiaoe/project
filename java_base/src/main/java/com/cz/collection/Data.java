201ca81a30659ece704b94615495d17982bf8389
aliancao <aliancao@gvt861.com>
2020-12-18 14:39:23 +0800

    list

---
+++ java_base/src/main/java/com/cz/collection/Data.java
@@ -0,0 +1,19 @@
+package com.cz.collection;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author alian
+ * @date 2020/12/8 下午 6:22
+ * @since JDK8
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@lombok.Data
+public class Data {
+
+    private Long id;
+
+    private String name;
+}
--- java_base/src/main/java/com/cz/collection/ListDemo.java
+++ java_base/src/main/java/com/cz/collection/ListDemo.java
@@ -1,7 +1,12 @@
 package com.cz.collection;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -29,5 +34,56 @@ public class ListDemo {
         for (Object o : list) {
             System.out.println(o);
         }
+    }
+
+    @Test
+    public void testIterator(){
+        List<Data> list = new ArrayList<>();
+        for (Long i = 1L; i < 6L; i++) {
+            list.add(new Data(i, "name_" + i));
+        }
+        System.out.println(list);
+//        final Iterator<Data> iterator = list.iterator();
+//        while (iterator.hasNext()) {
+//            Data next = iterator.next();
+//            if(next.getId() == 3L){
+//                iterator.remove();
+//            }
+//            if(next.getId() == 4L){
+////                next = new Data(7L, "haha");
+//                next.setName("xxxxx");
+//            }
+//        }
+
+//        Data data4 = new Data(4L, "asdasd");
+//        for (int i = 0; i < list.size(); i++) {
+//            final Data data = list.get(i);
+//            if(4 == data.getId()){
+//                list.set(i, data4);
+//            }
+//        }
+
+        for (Data data : list) {
+            if(data.getId() == 2){
+                data.setName("aaaaaaa");
+            }
+        }
+        System.out.println(list);
+    }
+
+    @Test
+    public void test2(){
+        List<List<Data>> data = new ArrayList<>();
+        List<Data> subData1 = new ArrayList<>();
+        subData1.add(new Data(1L, "1"));
+        subData1.add(new Data(2L, "2"));
+        List<Data> subData2 = new ArrayList<>();
+        subData2.add(new Data(3L, "3"));
+        subData2.add(new Data(4L, "4"));
+        data.add(subData1);
+        data.add(subData2);
+        final List<Data> collect = data.stream().flatMap(e -> e.stream()).collect(Collectors.toList());
+        System.out.println(collect);
+
     }
 }
