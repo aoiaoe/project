4c8c0e665222bf80f1742cf8feafd90a587b15f0
aliancao <aliancao@gvt861.com>
2021-01-05 10:51:33 +0800

    测试

--- ehcache/src/test/java/com/cz/ehcache/CacheTest.java
+++ ehcache/src/test/java/com/cz/ehcache/CacheTest.java
@@ -10,7 +10,9 @@ import org.ehcache.config.units.MemoryUnit;
 import org.junit.Test;
 
 import java.io.File;
+import java.text.Format;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -75,27 +77,77 @@ public class CacheTest {
         CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
                 .with(CacheManagerBuilder.persistence(new File(filePath, "myData")))
                 .withCache(cacheName,
-                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, Byte[].class,
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
                                 ResourcePoolsBuilder.newResourcePoolsBuilder()
 //                                        .heap(10L, EntryUnit.ENTRIES)
-                                        .offheap(2L, MemoryUnit.GB)
+                                        .offheap(1L, MemoryUnit.MB)
 //                                        .disk(10L, MemoryUnit.MB, true)
                         )
                 ).build(true);
-        Cache<Long, Byte[]> threeTieredCache = cacheManager.getCache(cacheName, Long.class, Byte[].class);
-        for (Long i = 1L; i < 50; i++) {
-            Byte[] b = new Byte[1024*1024* 20];
-            b[0] = 1;
-            b[1] = 2;
-            b[2] = 3;
-            threeTieredCache.put(i, b);
-        }
+        Cache<Long, String> threeTieredCache = cacheManager.getCache(cacheName, Long.class, String.class);
 
-        Byte[] byteArr = threeTieredCache.get(2L);
-        System.out.println(Arrays.toString(byteArr));
+        for (Long i = 1L; i <= 10; i++) {
+            char[] b = new char[512 * 500];
+            b[0] = (char)(64 + i.intValue());
+            for (int i1 = 1; i1 < 512 * 500; i1++) {
+                b[i1] = 'a';
+            }
+            System.out.println("------");
+            threeTieredCache.put(i, new String(b));
+            for (Long j = 1L; j <= 10; j++) {
+               String value = threeTieredCache.get(j);
+               if(value != null) {
+                   System.out.println(j + " - > " + value.substring(0, 20));
+               } else {
+                   System.out.println(j + " - > " + value);
+               }
+            }
+            System.out.println("------");
+        }
+        System.out.println("------");
+        System.out.println("------");
+        System.out.println("------");
+        System.out.println("------");
+        System.out.println("------");
         cacheManager.close();
         try {
             TimeUnit.SECONDS.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+    }
+
+    @Test
+    public void test3(){
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .with(CacheManagerBuilder.persistence(new File(filePath, "myData")))
+                .withCache(cacheName,
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+                                ResourcePoolsBuilder.newResourcePoolsBuilder()
+//                                        .heap(10L, EntryUnit.ENTRIES)
+                                        .offheap(10L, MemoryUnit.MB)
+//                                        .disk(10L, MemoryUnit.MB, true)
+                        )
+                ).build(true);
+        Cache<Long, String> threeTieredCache = cacheManager.getCache(cacheName, Long.class, String.class);
+
+        for (Long i = 0L; i < 10; i++) {
+            threeTieredCache.put(i, "value_" + i);
+        }
+        System.out.println("------");
+        for (long i = 0; i < 10L; i++) {
+            System.out.println(i + " : " + threeTieredCache.get(i));
+        }
+        System.out.println("------");
+        final Iterator<Cache.Entry<Long, String>> iterator = threeTieredCache.iterator();
+        while (iterator.hasNext()){
+            Long key = iterator.next().getKey();
+            if(key == 3 || key == 4){
+                threeTieredCache.remove(key);
+            }
+        }
+        System.out.println("------");
+        for (long i = 0; i < 10L; i++) {
+            System.out.println(i + " : " + threeTieredCache.get(i));
+        }
+        cacheManager.close();
     }
 
 }
