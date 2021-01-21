db033bd2bced9a8a10a6159a12300bb05f62080c
aliancao <aliancao@gvt861.com>
2021-01-21 16:25:29 +0800

    引入mybatis-plus

---
+++ spring_boot_test/src/main/java/com/cz/spring_boot_test/dao/DemoMapper.java
@@ -0,0 +1,16 @@
+package com.cz.spring_boot_test.dao;
+
+import com.cz.spring_boot_test.entity.Demo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author alian
+ * @since 2021-01-21
+ */
+public interface DemoMapper extends BaseMapper<Demo> {
+
+}
---
+++ spring_boot_test/src/main/java/com/cz/spring_boot_test/entity/Demo.java
@@ -0,0 +1,34 @@
+package com.cz.spring_boot_test.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author alian
+ * @since 2021-01-21
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Demo对象", description="")
+public class Demo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+
+}
