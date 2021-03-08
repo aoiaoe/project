ddae453cb77ce7d7d3382535bd7252fd448cd671
aliancao <aliancao@gvt861.com>
2021-03-08 16:07:28 +0800

    整合swagger文档

--- spring_cloud_provider_user/spring_cloud_provider_user_9105/src/main/java/com/cz/springcloudprovideruser/SpringCloudProviderUser9105Application.java
+++ spring_cloud_provider_user/spring_cloud_provider_user_9105/src/main/java/com/cz/springcloudprovideruser/SpringCloudProviderUser9105Application.java
@@ -3,9 +3,10 @@ package com.cz.springcloudprovideruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.cz")
 public class SpringCloudProviderUser9105Application {
 
     public static void main(String[] args) {
--- spring_cloud_provider_user/spring_cloud_provider_user_9105/src/main/java/com/cz/springcloudprovideruser/controller/UserController.java
+++ spring_cloud_provider_user/spring_cloud_provider_user_9105/src/main/java/com/cz/springcloudprovideruser/controller/UserController.java
@@ -3,7 +3,9 @@ package com.cz.springcloudprovideruser.controller;
 import com.cz.springcloud.api.UserApi;
 import com.cz.springcloud.entity.User;
 import com.cz.springcloudprovideruser.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,11 +16,13 @@ public class UserController implements UserApi {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "获取所有用户")
     @Override
     public List<User> findAll() {
         return this.userService.findAll();
     }
 
+    @ApiOperation(value = "获取指定用户")
     @Override
     public User get(Long id) {
         return this.userService.get(id);
