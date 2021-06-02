0f3457aeef83b5b66e78277a5b10c51754392c79
caozhen <caozhen@yiye.ai>
2021-06-02 17:45:11 +0800

    项目中配置HTTPS

---
+++ spring_boot_https_6900/src/main/java/com/cz/spring_boot_https/config/TomcatConfig.java
@@ -0,0 +1,43 @@
+package com.cz.spring_boot_https.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @Bean
+    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+        return factory;
+    }
+    private Connector createTomcatConnector() {
+        Connector connector = new
+                Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(6901);
+        connector.setSecure(false);
+        connector.setRedirectPort(port);
+        return connector;
+    }
+}
