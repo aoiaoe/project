6f909410063ab71e1f4347396e731d290c3c7e09
caozhen <caozhen@yiye.ai>
2021-06-03 14:32:39 +0800

    死信交换机实现延迟队列

---
+++ spring_boot_rabbitmq_8700/src/main/java/com/cz/spring_boot_rabbitmq/delayqueue/RabbitMqListener.java
@@ -0,0 +1,18 @@
+package com.cz.spring_boot_rabbitmq.delayqueue;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RabbitMqListener {
+
+    @RabbitListener(queues = {RabbitMqConfig.PROCESS_QUEUE},containerFactory = "containerFactory")
+    public void process(Channel channel, Message message){
+        String msg = new String(message.getBody());
+        log.info("接受消息:{}", msg);
+    }
+}
--- spring_boot_rabbitmq_8700/src/main/java/com/cz/spring_boot_rabbitmq/direct/DirectListenerDemo.java
+++ spring_boot_rabbitmq_8700/src/main/java/com/cz/spring_boot_rabbitmq/direct/DirectListenerDemo.java
@@ -1,5 +1,8 @@
 package com.cz.spring_boot_rabbitmq.direct;
 
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -12,7 +15,9 @@ import java.util.Map;
 @Component
 public class DirectListenerDemo {
 
-    @RabbitListener(queues = "direct_demo_queue")
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue("direct_demo_queue"), exchange = @Exchange(value = "default"), key = {"#"})
+    })
     public void process(@Payload String message,
                         @Headers Map<String, Object> headers,
                         @Header(value = AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey){
