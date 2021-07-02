bd53c26e74e134b5f50a04c51c75658c64450c23
caozhen <caozhen@yiye.ai>
2021-07-02 15:11:54 +0800

    update

--- algorithm/src/main/java/leetcode/_1_two_sum.java
+++ algorithm/src/main/java/leetcode/_1_two_sum.java
@@ -1,11 +1,11 @@
-package _1_two_sum;
+package leetcode;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 的那两个整数，并返回它们的数组下标。
  * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  * <p>
@@ -13,7 +13,7 @@ import java.util.Map;
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-class Solution {
+class _1_two_sum {
 
     public static void main(String[] args) {
         int[] nums = new int[]{3, 2, 4};
