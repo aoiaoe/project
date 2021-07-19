9c8060b3e18486a091d0ed0e4920b341b84ce58c
caozhen <caozhen@yiye.ai>
2021-07-19 15:40:18 +0800

    update

--- algorithm/src/main/java/leetcode/_3_LongestString.java
+++ algorithm/src/main/java/leetcode/_3_LongestString.java
@@ -5,6 +5,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
 public class _3_LongestString {
 
     public static void main(String[] args) {
