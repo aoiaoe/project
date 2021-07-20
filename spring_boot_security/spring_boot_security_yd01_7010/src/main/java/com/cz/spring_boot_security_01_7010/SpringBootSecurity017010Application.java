67b9035eae4719d8920082d573e9d0756ede11e8
aoiaoe <aoiaue@163.com>
2021-07-20 22:41:20 +0800

    [参考芋道源码]: springboot整合spring security demo1 使用内存用户简单入门

---
+++ spring_boot_security/spring_boot_security_yd01_7010/src/main/java/com/cz/spring_boot_security_01_7010/SpringBootSecurity017010Application.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_security_01_7010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurity017010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurity017010Application.class, args);
+    }
+
+}
---
+++ spring_boot_security/spring_boot_security_yd01_7010/src/test/java/com/cz/spring_boot_security_01_7010/SpringBootSecurity017010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_security_01_7010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurity017010ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
