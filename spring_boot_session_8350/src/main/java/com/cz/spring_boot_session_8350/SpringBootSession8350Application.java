697c59eedf22c56d73ced2ec4aee9754f8d45c42
caozhen <caozhen@yiye.ai>
2021-07-21 14:51:18 +0800

    [参考芋道源码]: springboot 整合Spring session

---
+++ spring_boot_session_8350/src/main/java/com/cz/spring_boot_session_8350/SpringBootSession8350Application.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_session_8350;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSession8350Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSession8350Application.class, args);
+    }
+
+}
---
+++ spring_boot_session_8350/src/test/java/com/cz/spring_boot_session_8350/SpringBootSession8350ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_session_8350;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSession8350ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
