d7dd0f3a151bf0d657e4bb43cc67973cf168c6a4
aoiaoe <aoiaue@163.com>
2021-07-24 14:24:23 +0800

    修改Redis序列化器

--- spring_boot_security/spring_boot_security_yd03_in_action_7012/src/main/java/com/cz/spring_boot_security_dy03_in_action_7012/config/RedisConfig.java
+++ spring_boot_security/spring_boot_security_yd03_in_action_7012/src/main/java/com/cz/spring_boot_security_dy03_in_action_7012/config/RedisConfig.java
@@ -1,27 +1,57 @@
 package com.cz.spring_boot_security_dy03_in_action_7012.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> stringObjectRedisTemplate(RedisConnectionFactory factory){
-        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(new ObjectMapper());
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
-        StringRedisSerializer stringSerializer = new StringRedisSerializer();
-        redisTemplate.setKeySerializer(stringSerializer);
-        redisTemplate.setHashKeySerializer(stringSerializer);
+    public RedisTemplate stringObjectRedisTemplate(RedisConnectionFactory factory){
+        RedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
 
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        // 设置键值序列化器
+        // 默认JDK的序列化器，序列化之后在redis客户端中不具可读性
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(factory);
+        stringRedisTemplate.setKeySerializer(stringRedisSerializer);
+        stringRedisTemplate.setValueSerializer(stringRedisSerializer);
+        stringRedisTemplate.setHashKeySerializer(stringRedisSerializer);
+        stringRedisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        return stringRedisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
+        RedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        // 设置键值序列化器
+        // 默认JDK的序列化器，序列化之后在redis客户端中不具可读性
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         return redisTemplate;
     }
 }
