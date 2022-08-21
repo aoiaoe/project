f8223ee855d8842b0b758d0d5930ff7bca055137
sephiroth <aoiaoe@163.com>
2022-08-21 15:14:59 +0800

    分模块管理

--- spring_cloud_consumer/spring_cloud_consumer_feign_hystrix_stream_9205/src/test/java/com/cz/springcloudconsumerfeignhystrixstream/SpringCloudConsumerFeignHystrixStream9205ApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.cz.springcloudconsumerfeignhystrixstream;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringCloudConsumerFeignHystrixStream9205ApplicationTests {
-
-}
