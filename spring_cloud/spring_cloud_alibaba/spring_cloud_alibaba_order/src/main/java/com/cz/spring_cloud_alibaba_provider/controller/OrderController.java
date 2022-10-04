9ff380eaf653cbb88d67e397516dc83ca147c5cf
sephiroth <aoiaoe@163.com>
2022-10-04 14:19:59 +0800

    feign拦截器

--- spring_cloud/spring_cloud_alibaba/spring_cloud_alibaba_order/src/main/java/com/cz/spring_cloud_alibaba_provider/controller/OrderController.java
+++ spring_cloud/spring_cloud_alibaba/spring_cloud_alibaba_order/src/main/java/com/cz/spring_cloud_alibaba_provider/controller/OrderController.java
@@ -3,12 +3,17 @@ package com.cz.spring_cloud_alibaba_provider.controller;
 import com.cz.spring_cloud_alibaba_api.domain.order.OrderVo;
 import com.cz.spring_cloud_alibaba_api.facade.order.OrderFacade;
 import com.cz.spring_cloud_alibaba_provider.config.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@Slf4j
 @RequestMapping(value = "/order")
 @RestController
 public class OrderController implements OrderFacade {
@@ -18,6 +23,9 @@ public class OrderController implements OrderFacade {
 
     @Override
     public List<OrderVo> userOrders(Long userId) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        final String client = request.getHeader("client");
+        log.info("[客户端]:{}", client);
         return this.orderService.userOrders(userId);
     }
 }
