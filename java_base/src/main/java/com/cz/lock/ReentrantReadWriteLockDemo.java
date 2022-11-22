bcec93c6e74f108ec647d3eda02e3d209ea1c90b
aoiaoe <aoiaue@163.com>
2022-11-22 14:26:02 +0800

    读写锁实验

--- java_base/src/main/java/com/cz/lock/ReentrantReadWriteLockDemo.java
+++ java_base/src/main/java/com/cz/lock/ReentrantReadWriteLockDemo.java
@@ -1,19 +1,27 @@
 package com.cz.lock;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 读读共享， 其他各情况互斥
  *
+ * t1、A获取读锁，持有读锁
+ * t2、B尝试获取写锁， 阻塞
+ * t3、C尝试获取读锁， 阻塞，因为CLA队列头为获取写锁的节点
+ *
  * @author alian
  * @date 2021/2/1 下午 12:17
  * @since JDK8
  */
+@Slf4j
 public class ReentrantReadWriteLockDemo {
 
     public static void main(String[] args) {
@@ -25,33 +33,35 @@ public class ReentrantReadWriteLockDemo {
         ReentrantReadWriteLock.ReadLock readLock = rwlock.readLock();
         ReentrantReadWriteLock.WriteLock writeLock = rwlock.writeLock();
 
-        List<String> list = new ArrayList<>();
+        StringBuffer sb = new StringBuffer();
 
-        new Thread(() -> {
+        Thread a = new Thread(() -> {
             readLock.lock();
             try {
-                System.out.println(System.currentTimeMillis() + "-" + Thread.currentThread().getName() + " got readLock");
-                list.stream().forEach(System.out::println);
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                log.info("获取到读锁");
+                TimeUnit.HOURS.sleep(1);
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 readLock.unlock();
             }
-            System.out.println(System.currentTimeMillis() + "-" + Thread.currentThread().getName() + " rel readLock");
-        }, "A").start();
+            log.info("释放读锁");
+        }, "A");
+        a.start();
 
-        new Thread(() -> {
+        Thread c = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("阻塞获取写锁");
             writeLock.lock();
             try {
-                System.out.println(System.currentTimeMillis() + "-" + Thread.currentThread().getName() + " got writeLock");
-                list.add("asdsa");
+                log.info("获取到写锁");
+                sb.append("asdsa");
                 try {
-                    TimeUnit.SECONDS.sleep(2);
+                    TimeUnit.SECONDS.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -60,25 +70,28 @@ public class ReentrantReadWriteLockDemo {
             } finally {
                 writeLock.unlock();
             }
-            System.out.println(System.currentTimeMillis() + "-" + Thread.currentThread().getName() + " rel writeLock");
-        }, "C").start();
+            log.info("释放写锁");
+        }, "C");
+        c.start();
 
         new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("阻塞获取读锁");
+            readLock.lock();
             readLock.lock();
             try {
-                System.out.println(System.currentTimeMillis() + "-" + Thread.currentThread().getName() + " got readLock");
-                list.stream().forEach(System.out::println);
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+               log.info("获取到读锁:{}", sb.toString());
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 readLock.unlock();
+                readLock.unlock();
             }
-            System.out.println(System.currentTimeMillis() + "-" + Thread.currentThread().getName() + " rel readLock");
+            log.info("释放读锁");
         }, "B").start();
 
 
