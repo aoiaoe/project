9d704612eec6f4d5504844e2d2ec4e22fddff2f4
sephiroth <aoiaoe@163.com>
2023-02-17 20:39:01 +0800

    算法

--- algorithm/src/main/java/niukewang/list/_3_ReverseKGroup.java
+++ algorithm/src/main/java/niukewang/list/_3_ReverseKGroup.java
@@ -16,15 +16,85 @@ package niukewang.list;
  */
 public class _3_ReverseKGroup {
 
+    public static void main(String[] args) {
+        ListNode head = ListNode.create(1);
+        head = reverseKGroup(head, 2);
+        ListNode.disp(head);
+    }
     /**
-     *
+     * 另一种思路1： 每次找到前面的K个节点，然后细化成反转链表问题
      * @param head ListNode类
      * @param k int整型
      * @return ListNode类
      */
     public static ListNode reverseKGroup (ListNode head, int k) {
         // write code here
+        if(head == null || k < 2){
+            return head;
+        }
+        ListNode sentinel = head;
+        // 反转后的总链表
+        ListNode xEnd = null;
+        // 反转后的每一组的子链表头结点
+        ListNode y = null;
+        // 反转后的每一组的子链表尾结点
+        ListNode yEnd = null;
+        ListNode temp = null;
+        int count = 0;
+        // 是否是第一组
+        boolean first = true;
+        while (sentinel != null){
+            // 检查长度, 不够则跳出循环
+            if(count == 0 && !checkLength(sentinel, k)){
+                break;
+            }
+            // 每一组开始,记录翻转后链表的结束节点
+            if(count == 0){
+                y = yEnd = sentinel;
+                sentinel = sentinel.next;
+                y.next = null;
+                count++;
+                continue;
+            }
+            // 翻转节点
+            temp = sentinel;
+            sentinel = sentinel.next;
+            temp.next = y;
+            y = temp;
 
-        return null;
+            count++;
+            if(count == k){
+                count = 0;
+                // 如果是,第一轮循环, 则更新头结点
+                if(first){
+                    head = y;
+                    first = false;
+                } else{
+                    // 每一组的最后一次循环, 减翻转后的节点拼接到总链表后面
+                    xEnd.next = y;
+                }
+                // 记录反转后总链表的结束节点
+                xEnd = yEnd;
+            }
+        }
+        // 将剩下的不足的几点，拼接到总链表的后面
+        if (sentinel != null && xEnd != null){
+            xEnd.next = sentinel;
+        }
+        return head;
+    }
+
+    // 校验链表长度
+    public static boolean checkLength(ListNode head, int k){
+        int index = 0;
+        ListNode sentinel = head;
+        while (sentinel != null){
+            index++;
+            if(index == k){
+                return true;
+            }
+            sentinel = sentinel.next;
+        }
+        return false;
     }
 }
---
+++ algorithm/src/main/java/niukewang/list/_5_MergeKLists.java
@@ -0,0 +1,18 @@
+package niukewang.list;
+
+import java.util.ArrayList;
+
+/**
+ * 合并k个已排序的链表
+ * 合并 k个升序的链表并将结果作为一个升序的链表返回其头节点。
+ *
+ * 数据范围：节点总数0≤n≤5000，每个节点的val满足∣val∣<=1000
+ * 要求：时间复杂度O(nlogn)
+ */
+public class _5_MergeKLists {
+    public ListNode mergeKLists(ArrayList<ListNode> lists) {
+        ListNode head = null;
+
+        return head;
+    }
+}
