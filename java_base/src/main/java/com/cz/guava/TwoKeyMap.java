026a1e34dda6176eb7c61b65ccb55f7d3c00337a
aoiaoe <aoiaue@163.com>
2023-02-23 16:02:19 +0800

    guava

---
+++ java_base/src/main/java/com/cz/guava/TwoKeyMap.java
@@ -0,0 +1,17 @@
+package com.cz.guava;
+
+import com.google.common.collect.HashBasedTable;
+
+/**
+ * @author jzm
+ * @date 2023/2/23 : 15:59
+ */
+public class TwoKeyMap {
+
+    public static void main(String[] args) {
+        HashBasedTable<Object, Object, Object> testTable = HashBasedTable.create();
+        testTable.put("cz", "jzm", 20);
+        testTable.put("wxf", "fwe", 20);
+        System.out.println(testTable.row("cz").values());
+    }
+}
