74e8456715c298f89bb8ab39724cf80fe9841153
aoiaoe <aoiaue@163.com>
2023-04-04 16:16:30 +0800

    springboot整合cannal同步数据

---
+++ spring_boot/spring_boot_cannal/src/main/java/com/cz/spring_boot_cannal/SpringBootCannalApplication.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_cannal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCannalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCannalApplication.class, args);
+	}
+
+}
