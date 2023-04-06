99a0446cf71c2e6d2be29031543d65cb48a1c039
aoiaoe <aoiaue@163.com>
2023-04-06 13:01:29 +0800

    springboot整合canal

---
+++ spring_boot/spring_boot_canal/src/main/java/com/cz/spring_boot_canal/SpringBootCannalApplication.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCannalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCannalApplication.class, args);
+	}
+
+}
---
+++ spring_boot/spring_boot_canal/src/test/java/com/cz/spring_boot_canal/SpringBootCannalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.spring_boot_canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCannalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
