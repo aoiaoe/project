b5b3dac11913c63686aed0a36081e3009210863e
aoiaoe <aoiaue@163.com>
2023-04-21 18:00:33 +0800

    update

--- spring_boot/spring_boot_mix/src/main/java/com/cz/spring_boot_mix/event/MyEventListener.java
+++ spring_boot/spring_boot_mix/src/main/java/com/cz/spring_boot_mix/event/MyEventListener.java
@@ -1,13 +1,15 @@
 package com.cz.spring_boot_mix.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class MyEventListener implements ApplicationListener<MyEvent> {
 
     @Override
     public void onApplicationEvent(MyEvent event) {
-        System.out.println("my event listener :" + event.getSource().toString());
+        log.info("my event listener :{}",event.getSource().toString());
     }
 }
