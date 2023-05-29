5e58ab452de9e2de596e45869c26218b8fe83303
aoiaoe <aoiaue@163.com>
2023-05-29 18:18:25 +0800

    import bean demo

--- spring_boot/spring_boot_mix/src/main/java/com/cz/spring_boot_mix/importannotation/TestC.java
+++ spring_boot/spring_boot_mix/src/main/java/com/cz/spring_boot_mix/importannotation/TestC.java
@@ -1,11 +1,17 @@
 package com.cz.spring_boot_mix.importannotation;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author jzm
  * @date 2022/1/13 : 10:12
  */
+@Slf4j
 public class TestC {
 
+    public TestC(){
+        log.info("初始化TestC");
+    }
     public void testC(){
         System.out.println("----> testC");
     }
--- spring_boot/spring_boot_mix/src/main/java/com/cz/spring_boot_mix/importannotation/TestD.java
+++ spring_boot/spring_boot_mix/src/main/java/com/cz/spring_boot_mix/importannotation/TestD.java
@@ -1,11 +1,17 @@
 package com.cz.spring_boot_mix.importannotation;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author jzm
  * @date 2022/1/13 : 10:14
  */
+@Slf4j
 public class TestD {
 
+    public TestD(){
+        log.info("初始化TestD");
+    }
     public void testD(){
         System.out.println("----> testD");
     }
