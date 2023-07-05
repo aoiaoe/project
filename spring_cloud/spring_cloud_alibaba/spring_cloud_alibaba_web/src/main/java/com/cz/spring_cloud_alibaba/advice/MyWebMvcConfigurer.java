813fbb0a09fcb0158d864fc2f0bd6ccae625427d
jzm <535580204@qq.com>
2023-07-05 11:37:32 +0800

    将json序列化起移到队列头，解决controller方法直接返回string时报错的情况

---
+++ spring_cloud/spring_cloud_alibaba/spring_cloud_alibaba_web/src/main/java/com/cz/spring_cloud_alibaba/advice/MyWebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.cz.spring_cloud_alibaba.advice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+    /**
+     * 将json序列化起移到队列头，解决controller方法直接返回string时报错的情况
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new MappingJackson2HttpMessageConverter());
+    }
+}
