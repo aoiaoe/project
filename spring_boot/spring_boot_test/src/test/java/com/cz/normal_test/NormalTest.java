d791187645cd3d3cef50cb71f15e54815f26263c
jzm <535580204@qq.com>
2023-10-20 12:10:07 +0800

    update

--- spring_boot/spring_boot_test/src/test/java/com/cz/normal_test/NormalTest.java
+++ spring_boot/spring_boot_test/src/test/java/com/cz/normal_test/NormalTest.java
@@ -3,6 +3,10 @@ package com.cz.normal_test;
 import com.cz.spring_boot_test.utils.DateUtils;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * 普通的单元测试，如只测试算法之类的，不需要用到spring的组件
  * 就可以在测试方法上加入@Test注解直接启动测试
@@ -17,4 +21,18 @@ public class NormalTest {
         assert "2020-12-20".equals(result);
     }
 
+    @Test
+    public void testListIte(){
+        List<String> list = new ArrayList(){{
+            add("a");
+            add("b");
+            add("c");
+        }};
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            list.add("d");
+            System.out.println(iterator.next());
+        }
+    }
 }
