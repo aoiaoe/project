f3a189d7a5e579e6b5cf7ca9841e5168c8938a84
jzm <535580204@qq.com>
2023-11-14 15:47:49 +0800

    单元测试

--- spring_boot/spring_boot_test/src/test/java/com/cz/spring_boot_test/MapControllerMockTest.java
+++ spring_boot/spring_boot_test/src/test/java/com/cz/spring_boot_test/MapControllerMockTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,8 +25,24 @@ public class MapControllerMockTest {
 
     @Test
     public void test() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/map/9")).andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/map/key/9")).andDo(MockMvcResultHandlers.print())
+                // 断言
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                // 打印请求响应信息。
+                .andDo(MockMvcResultHandlers.print());
     }
+
+    @Test
+    public void test1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/map/json")
+                .header("token", "abc")
+                .param("id", "111")
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("token").value("abc"))
+                .andReturn();
+    }
+
 
 }
