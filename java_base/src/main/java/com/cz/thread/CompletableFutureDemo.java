0c2108b63f80d972d5e49c9212811d31f7c38b7b
jzm <535580204@qq.com>
2024-03-07 15:55:50 +0800

    CompletableFuture父子任务使用同一个线程池带来的bug

--- java_base/src/main/java/com/cz/thread/CompletableFutureDemo.java
+++ java_base/src/main/java/com/cz/thread/CompletableFutureDemo.java
@@ -6,10 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -108,5 +107,45 @@ public class CompletableFutureDemo {
                 .stream()
                 .map(CompletableFuture::join).collect(Collectors.toList());
         log.info("task end");
+    }
+
+
+    public static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+            3,50,5, TimeUnit.SECONDS,
+            new LinkedBlockingDeque<>(10),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy()
+    ) ;
+
+    /**
+     * Completable 父子任务 + 使用同一个线程池， 可能导致死锁
+     * @throws Exception
+     */
+    @Test
+    public void testDeadLock() throws Exception {
+
+        List<CompletableFuture<Void>> list = new ArrayList<>(0) ;
+        for(int i = 0 ; i < 3 ; i++) {
+            // 2、线程池中就3个空闲线程，因为做了 sleep，所以 3个资源都给了父任务
+            CompletableFuture<Void> parentTask = CompletableFuture.runAsync(() -> {
+                try {
+                    System.out.println("父任务执行了：" + Thread.currentThread().getName());
+                    Thread.sleep(10);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                // 3、子任务在等待父任务释放资源，父任务在等待子任务执行完，死锁
+                CompletableFuture<Void> childTask = CompletableFuture.runAsync(() -> {
+                    System.out.println("子任务执行了：" + Thread.currentThread().getName());
+                }, threadPoolExecutor);
+                childTask.join() ;
+
+            }, threadPoolExecutor);
+            list.add(parentTask) ;
+        }
+        // 1、开始创建3个异步任务并执行
+        CompletableFuture.allOf(list.toArray(new CompletableFuture[0])).get();
+        threadPoolExecutor.shutdown();
+        System.out.println("exit");
     }
 }
