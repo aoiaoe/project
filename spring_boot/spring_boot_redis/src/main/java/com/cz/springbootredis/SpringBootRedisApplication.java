ad8fc9f25927fc9ea878229ccbdaa52673bf9f38
jzm <535580204@qq.com>
2024-06-13 22:57:37 +0800

    update

--- spring_boot/spring_boot_redis/src/main/java/com/cz/springbootredis/SpringBootRedisApplication.java
+++ spring_boot/spring_boot_redis/src/main/java/com/cz/springbootredis/SpringBootRedisApplication.java
@@ -2,7 +2,9 @@ package com.cz.springbootredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SpringBootRedisApplication {
 
