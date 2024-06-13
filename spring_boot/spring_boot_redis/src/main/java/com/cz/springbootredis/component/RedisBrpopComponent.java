fee0785e454c3b2448c985957b19c2274fde9a3e
jzm <535580204@qq.com>
2024-06-13 22:58:08 +0800

    redis brpop需使用超时时间

--- spring_boot/spring_boot_redis/src/main/java/com/cz/springbootredis/component/RedisBrpopComponent.java
+++ spring_boot/spring_boot_redis/src/main/java/com/cz/springbootredis/component/RedisBrpopComponent.java
@@ -27,13 +27,15 @@ public class RedisBrpopComponent {
      * 但是等待消息中的连接，会被判定为空闲连接
      * 如果线程一直阻塞在哪里，Redis 的客户端连接就成了闲置连接，闲置过久(spring.redis.timeout配置)，服务器一般会主动断开连接，减少闲置资源占用。
      * 这个时候blpop/brpop会抛出异常来。
+     * 所以需要使用带超时的brpop方法，并且需要比spring.redis.timeout配置短
      */
-    @PostConstruct
+//    @PostConstruct //单线程执行，会阻塞项目启动
     public void testBrpop(){
         logger.info("测试brpop");
         try {
             while (true) {
-                Object testList = stringRedisTemplate.opsForList().rightPop("testList", Duration.ofMinutes(10));
+//                Object testList = stringRedisTemplate.opsForList().rightPop("testList", Duration.ofMinutes(10));
+                Object testList = stringRedisTemplate.opsForList().rightPop("testList", Duration.ofSeconds(5));
                 logger.info("从list中获取消息：{}", testList);
             }
         }catch (Exception e){
