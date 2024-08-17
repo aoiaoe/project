d48094a94d4c91452a6343b8c833aeb4164d6a81
jzm <535580204@qq.com>
2024-08-17 21:32:01 +0800

    update

--- spring_boot/spring_boot_mybatis_dynamic_ds_8699/src/main/java/com/cz/spring_boot_mybatis_dynamic_ds/config/MyBatisDynamicDsConfig.java
+++ spring_boot/spring_boot_mybatis_dynamic_ds_8699/src/main/java/com/cz/spring_boot_mybatis_dynamic_ds/config/MyBatisDynamicDsConfig.java
@@ -1,47 +1,17 @@
 package com.cz.spring_boot_mybatis_dynamic_ds.config;
 
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
-import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
-import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
-import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
-import com.baomidou.mybatisplus.autoconfigure.SpringBootVFS;
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.baomidou.mybatisplus.core.config.GlobalConfig;
-import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
-import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
-import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import lombok.AllArgsConstructor;
-import org.apache.ibatis.scripting.LanguageDriver;
-import org.apache.ibatis.session.ExecutorType;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.shardingsphere.shardingjdbc.jdbc.core.datasource.ShardingDataSource;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 /**
  * @author jzm
@@ -60,8 +30,7 @@ public class MyBatisDynamicDsConfig {
     List<DataSource> dataSources;
     @PostConstruct
     public void init(){
-//        dynamicRoutingDataSource.addDataSource("shard", shardingDataSource);
-//        applicationContext.
+        // 奇葩，我为啥要将分库数据源加入到动态数据源？
         DynamicRoutingDataSource bean = applicationContext.getBean(DynamicRoutingDataSource.class);
         bean.addDataSource("shard", applicationContext.getBean(ShardingDataSource.class));
         System.out.println();
